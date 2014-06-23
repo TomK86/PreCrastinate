81d49a0a59720aeecc98fd16e98ecf4d6ea4ad08
Tom Kelly <thke7920@colorado.edu>
2014-06-23 09:39:10 -0600

    .gitignore is now working

---
+++ src/com/csci3308/precrastinate/AddTask.java
@@ -0,0 +1,176 @@
+package com.csci3308.precrastinate;
+
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import android.view.View.OnClickListener;
+
+import com.csci3308.precrastinate.R;
+
+
+
+public class AddTask extends ActionBarActivity {
+	
+	EditText duedate;
+	Spinner groupSpinner;
+	RatingBar priority;
+	EditText taskTitleName;
+	List<String> listGroupHeaders;
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_task);
+		
+		listGroupHeaders = new ArrayList<String>();
+		for(int i = 0; i < MainActivity.listGroupObjs.size(); i++)
+			listGroupHeaders.add(MainActivity.listGroupObjs.get(i).getName());
+		
+		// Assign Due Date
+	    duedate = (EditText) findViewById(R.id.due_date);
+		
+		 // EditText click listener
+        duedate.setOnClickListener(new OnClickListener() {
+       	
+       	@Override
+            public void onClick(View v) {
+       		Toast.makeText(getApplicationContext(),
+           		"Please select the deadline or due date for this task",
+           		Toast.LENGTH_LONG).show();
+           	Calendar mCurrentDate = Calendar.getInstance();
+           	int day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+           	int month = mCurrentDate.get(Calendar.MONTH);
+           	int year = mCurrentDate.get(Calendar.YEAR);
+           	DatePickerDialog mDatePicker;
+           	mDatePicker = new DatePickerDialog(AddTask.this, new DatePickerDialog.OnDateSetListener() {
+           		@Override
+                public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                duedate.setText((selectedMonth + 1) + " / " + selectedDay + " / " + selectedYear);
+                
+                }
+           	}, year, month, day);
+               mDatePicker.setTitle("Select Due Date");
+               mDatePicker.show();
+               
+           }
+       	
+       	
+        });
+
+		// Create Spinner
+		groupSpinner = (Spinner) findViewById(R.id.groups);
+		ArrayAdapter<String> groupsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listGroupHeaders);
+		groupsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groupSpinner.setAdapter(groupsAdapter);
+        
+        groupSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { 
+        	public void onItemSelected(AdapterView<?> adapterView, 
+            View view, int i, long l) { 
+            }
+              // If no option selected
+        	public void onNothingSelected(AdapterView<?> arg0) {
+        		// TODO Auto-generated method stub
+        
+        	} 
+        	
+
+        });
+        
+		// Assign Priority
+		priority = (RatingBar) findViewById(R.id.priority);
+		priority.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+
+			@Override
+			public void onRatingChanged(RatingBar ratingBar, float rating,
+					boolean fromUser) {
+			}
+		});
+		
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	/**
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	} */
+
+	/** Called when the user clicks the Save button */
+	
+	public void saveTaskButton(View view) {
+		
+		taskTitleName = (EditText) findViewById(R.id.new_task);
+		String taskTitle = taskTitleName.getText().toString();
+		String str = duedate.getText().toString();
+		String dueDateName = str.replace(" / ", "");
+		long chosenDate = Long.valueOf(dueDateName);
+		String chosenGroup = groupSpinner.getSelectedItem().toString();
+		int index = listGroupHeaders.indexOf(chosenGroup);
+		float chosenRating = priority.getRating();
+		
+		
+		MainActivity.saveTaskData(taskTitle, chosenDate, chosenRating, index, false);
+		
+		Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+		
+		finish();
+	}
+
+
+	
+}
+	
+
+	
+
+		
+
+
---
+++ src/com/csci3308/precrastinate/Preferences.java
@@ -0,0 +1,201 @@
+package com.csci3308.precrastinate;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.TimePicker;
+import android.widget.EditText;
+
+import com.csci3308.precrastinate.R;
+
+public class Preferences extends Activity {
+ 
+	ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    EditText remTime;
+    EditText grpNames;
+    RadioGroup grpColors;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        
+        // get the parent view
+        expListView = (ExpandableListView) findViewById(R.id.groupExpList);
+        
+        // get the child views
+        remTime = (EditText) findViewById(R.id.remTimeEdit);
+        
+        grpNames = (EditText) findViewById(R.id.grpName);
+        
+        grpColors = (RadioGroup) findViewById(R.id.grpColor);
+        
+        // get expandable list adapter
+        listAdapter = new ExpandableListAdapter(this);
+ 
+        // set expandable list adapter
+        expListView.setAdapter(listAdapter);
+        
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+ 
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                    int groupPosition, long id) {
+                return false;
+            }
+        });
+ 
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+        	int previousGroup = -1;
+ 
+            @Override
+            public void onGroupExpand(int groupPosition) {
+            	if((groupPosition != previousGroup) && (previousGroup != -1))
+                    expListView.collapseGroup(previousGroup);
+                previousGroup = groupPosition;
+            }
+        });
+ 
+        // Listview Group collapsed listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+ 
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+            	//Toast.makeText(getApplicationContext(),
+                		//"'" + MainActivity.listGroupObjs.get(groupPosition).getName() + "' group saved",
+                        //Toast.LENGTH_SHORT).show();
+            }
+        });
+ 
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+ 
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                    int groupPosition, int childPosition, long id) {
+                //if(v.getTag() == "group save button") {
+                	//String name = listAdapter.getChildName(groupPosition, null);
+                	//MainActivity.listGroupObjs.get(groupPosition).setName(name);
+                	//listAdapter.notifyDataSetChanged();
+                	//return true;
+                //}
+                //else
+                	return false;
+            }
+        });
+    }
+    
+    public void onReminderTimeClicked(View v) {
+		Toast.makeText(getApplicationContext(),
+    			"Please select the time of day you wish to receive reminders",
+    			Toast.LENGTH_LONG).show();
+    	Calendar mCurrentTime = Calendar.getInstance();
+    	int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
+    	int minute = 0;
+		if(hour == 23)
+    		hour = 0;
+		else
+			hour++;
+    	TimePickerDialog mTimePicker;
+    	mTimePicker = new TimePickerDialog(Preferences.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+            	if(selectedHour < 12) {
+            		if(selectedHour == 0)
+            			selectedHour = 12;
+            		if(selectedMinute < 10)
+            			remTime.setText(selectedHour + ":0" + selectedMinute + " AM");
+            		else
+            			remTime.setText(selectedHour + ":" + selectedMinute + " AM");
+            	}
+            	else {
+            		if(selectedHour > 12)
+            			selectedHour -= 12;
+            		if(selectedMinute < 10)
+            			remTime.setText(selectedHour + ":0" + selectedMinute + " PM");
+            		else
+            			remTime.setText(selectedHour + ":" + selectedMinute + " PM");
+            	}
+            }
+    	}, hour, minute, false);
+        mTimePicker.setTitle("Select Reminder Time");
+        mTimePicker.show();
+    }
+    
+	public void onRadioButtonClicked(View view) {
+	    // Is the button now checked?
+	    boolean checked = ((RadioButton) view).isChecked();
+	    int position = (Integer) ((RadioGroup) view.getParent()).getTag();
+	    // Check which radio button was clicked
+	    switch(view.getId()) {
+	        case R.id.color0:
+	            if (checked)
+	                MainActivity.listGroupObjs.get(position).setColor(0);
+	            break;
+	        case R.id.color1:
+	            if (checked)
+	            	MainActivity.listGroupObjs.get(position).setColor(1);
+	            break;
+	        case R.id.color2:
+	            if (checked)
+	            	MainActivity.listGroupObjs.get(position).setColor(2);
+	            break;
+	        case R.id.color3:
+	            if (checked)
+	            	MainActivity.listGroupObjs.get(position).setColor(3);
+	            break;
+	        case R.id.color4:
+	            if (checked)
+	            	MainActivity.listGroupObjs.get(position).setColor(4);
+	            break;
+	    }
+	    listAdapter.notifyDataSetChanged();
+	    Toast.makeText(getApplicationContext(),
+        		"'" + MainActivity.listGroupObjs.get(position).getName() + "' group color saved",
+                Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onGrpSaveBtnClicked(View view) {
+		int position = (Integer) view.getTag();
+		EditText grpName = (EditText) view.getRootView().findViewById(R.id.grpName);
+		//RadioGroup grpColor = (RadioGroup) view.getRootView().findViewById(R.id.grpColor);
+    	String name = grpName.getText().toString();
+    	//int color = grpColor.indexOfChild(grpColor.findViewById(grpColor.getCheckedRadioButtonId()));
+    	MainActivity.listGroupObjs.get(position).setName(name);
+    	//MainActivity.listGroupObjs.get(position).setColor(color);
+    	listAdapter.notifyDataSetChanged();
+    	Toast.makeText(getApplicationContext(),
+        		"'" + MainActivity.listGroupObjs.get(position).getName() + "' group name saved",
+                Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onAddGrpBtnClicked(View view) {
+		int key = MainActivity.listGroupObjs.size();
+		MainActivity.listGroupObjs.put(key, new Group("New Group", 0));
+		listAdapter.notifyDataSetChanged();
+		Toast.makeText(getApplicationContext(),
+        		"New group added to list",
+                Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onPrefSaveBtnClicked(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+	}
+    
+}
